2db51efd73a26d45cd03fa796077a77daf2fb4af
goto florian <gotoflorian.pro@gmail.com>
2018-04-04 22:21:07 +0200

    create BurgerBuilder container

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
   render() {
     return (
       <Layout>
-          <p>Tooooooooo</p>
+          <BurgerBuilder />
       </Layout>
     );
   }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 
+import classes from "./Layout.css";
 import Aux from "../../hoc/Aux";
 
 const Layout = ({ children }) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {children}
         </main>
     </Aux>
