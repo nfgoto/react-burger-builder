5bf2bd7839361700bc6e5207516d9e827e1e133f
goto florian <gotoflorian.pro@gmail.com>
2018-04-08 00:44:47 +0200

    add sideDrawer close logic

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./Layout.css";
 import Aux from "../../hoc/Aux";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = ({ children }) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        });
+    }
+
+    render(){
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} 
+                            closed={this.sideDrawerHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+};
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,17 +3,32 @@ import React from "react";
 import classes from "./SideDrawer.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../../Navigation/NavigationItems/NavigationItems";
+import BackDrop from "../../UI/BackDrop/BackDrop";
+import Aux from "../../../hoc/Aux";
 
-
-const SideDrawer = ({ link, active,  children }) => (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+const SideDrawer = ({ link, active, closed, open, children }) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close ];
+    
+    // Is the side drawer open ?
+    if (open) {
+        // remove Close CSS class
+        attachedClasses.pop();
+        attachedClasses.push(classes.Open);
+    }
+    
+    return (
+        <Aux>
+            <BackDrop show={open} clicked={closed}/>
+            <div className={attachedClasses.join(' ')}>
+            
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />    
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />    
-            </nav>
-        </div>
-    );
+        </Aux>
+    )};
 
 export default SideDrawer;
