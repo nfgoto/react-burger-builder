6ed1c3e1c3c7ed7f7d9119d0ffd17170fafba91d
goto florian <gotoflorian.pro@gmail.com>
2018-04-08 00:10:40 +0200

    add sideDrawer component for mobile viewport

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from "react";
 import classes from "./Layout.css";
 import Aux from "../../hoc/Aux";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = ({ children }) => (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {children}
         </main>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -8,8 +8,10 @@ const Toolbar = ({ a }) => {
     return (
         <header className={classes.Toolbar}>
             <div>MENU</div>
-            <Logo />
-            <nav>
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
+            <nav className={classes.DesktopOnly}>
                 <NavigationItems />
             </nav>
         </header>
