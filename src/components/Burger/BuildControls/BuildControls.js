30b2e079a6394aa20c9d1a820a38a2bf1ede344d
goto florian <gotoflorian.pro@gmail.com>
2018-04-07 13:37:54 +0200

    add total price in build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -6,28 +6,30 @@ import ig from "../BurgerIngredients/ingredientTypes";
 
 // types need to match the ones in burger ingredient
 const controls = [
-    {label: 'Salad', type: ig.SALAD},
-    {label: 'Cheese', type: ig.CHEESE},
-    {label: 'Meat', type: ig.MEAT},
-    {label: 'Bacon', type: ig.BACON}
+  { label: "Salad", type: ig.SALAD },
+  { label: "Cheese", type: ig.CHEESE },
+  { label: "Meat", type: ig.MEAT },
+  { label: "Bacon", type: ig.BACON }
 ];
 
-const BuildControls = ({ ingredientAdded, ingredientRemoved, disabled }) => {
-    return (
-        <div className={classes.BuildControls}>
-            {controls.map( (control, idx) => {
-                return (
-                <div key={control.type + idx}>
-                    <BuildControl 
-                        label={control.label}
-                        added={() => ingredientAdded(control.type)}
-                        removed={() => ingredientRemoved(control.type)}
-                        disabled={disabled[control.type]}/>
-                </div>
-            );
-            })}
-        </div>
-    );
+const BuildControls = ({ ingredientAdded, ingredientRemoved, disabled, price }) => {
+  return (
+    <div className={classes.BuildControls}>
+      <p><strong>Current Price: {price.toFixed(2)}</strong></p>
+      {controls.map((control, idx) => {
+        return (
+          <div key={control.type + idx}>
+            <BuildControl
+              label={control.label}
+              added={() => ingredientAdded(control.type)}
+              removed={() => ingredientRemoved(control.type)}
+              disabled={disabled[control.type]}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default BuildControls;
