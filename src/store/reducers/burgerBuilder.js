5df299b8e498cedd415fee6aec8b95b76aaf97ea
goto florian <gotoflorian.pro@gmail.com>
2018-05-20 14:58:10 +0200

    refactor reducers

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,13 +1,14 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "./utility";
 
 import igType from "../../components/Burger/BurgerIngredient/ingredientTypes";
 
 const INGREDIENT_PRICES = {
-    [igType.SALAD]: 0.5,
-    [igType.CHEESE]: 0.8,
-    [igType.MEAT]: 3.5,
-    [igType.BACON]: 2
-  };
+  [igType.SALAD]: 0.5,
+  [igType.CHEESE]: 0.8,
+  [igType.MEAT]: 3.5,
+  [igType.BACON]: 2
+};
 
 const initialState = {
   ingredients: null,
@@ -15,43 +16,47 @@ const initialState = {
   error: false
 };
 
+const addIngredient = (state, action) => {
+  const updatedIngredients = updateObject(state, {
+    ingredients: {
+      ...state.ingredients,
+      [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+    },
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+  });
+  return updatedIngredients;
+};
+
+const removeIngredient = (state, action) => {
+  return updateObject(state, {
+    ingredients: {
+      ...state.ingredients,
+      [action.ingredientName]: --state.ingredients[action.ingredientName]
+    },
+    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+  });
+}
+
+const setIngredients = (state, action) => {
+  return updateObject(state, {
+    ingredients: action.ingredients,
+    totalPrice: 10,
+    // in case there were an error previously
+    error: false
+  });
+};
+
+const fetchIngredientsFailed = (state, action) => {
+  return updateObject(state, { error: true });
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.SET_INGREDIENTS:
-      return{
-        ...state,
-        ingredients: action.ingredients,
-        totalPrice:10,
-        // in case there were an error previously
-        error: false
-      };
-    case actionTypes.FETCH_INGREDIENTS_FAILED:
-      return{
-        ...state,
-        error: true
-      };
-    case actionTypes.ADD_INGREDIENT:
-      // this is how to create a DEEP CLONE of an object
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-        },
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-      };
-    case actionTypes.REMOVE_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: --state.ingredients[action.ingredientName]
-        },
-        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
-
-      };
-    default:
-      return state;
+    case actionTypes.SET_INGREDIENTS: return setIngredients(state, action);
+    case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngredientsFailed(state, action);
+    case actionTypes.ADD_INGREDIENT: return addIngredient(state,action);
+    case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action);
+    default: return state;
   }
 };
 
--- src/store/reducers/orders.js
+++ src/store/reducers/orders.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "./utility";
 
 const initialState = {
   orders: [],
@@ -7,50 +8,64 @@ const initialState = {
   error: null
 };
 
-const order = (state = initialState, action) => {
-  switch (action.type) {
-    case actionTypes.PURCHASE_INIT:
-      return {
-        ...state,
-        purchased: false
-      };
-    case actionTypes.PURCHASE_BURGER_START:
-      return {
-        ...state,
-        loading: false
-      };
-    case actionTypes.PURCHASE_BURGER_SUCCESS:
-      const newOrder = { id: action.newOrderId, ...action.orderData };
-      return {
-        ...state,
+const purchaseInit = (state, action) => {
+  return updateObject(state, {
+    // turn back to false after pevious purchase done
+    purchased: false
+  });
+}
+
+const purchaseBurgerStart = (state, action) => {
+  return updateObject(state, {
+    loading: false
+  });
+}
+
+const purchaseBurgerSuccess = (state, action) => {
+  const newOrder = { id: action.newOrderId, ...action.orderData };
+      return updateObject(state, {
         loading: false,
         purchased: true,
         orders: [...state.orders, newOrder]
-      };
-    case actionTypes.PURCHASE_BURGER_FAIL:
-      return {
-        ...state,
-        loading: false,
-        error: action.error
-      };
-    case actionTypes.FETCH_ORDERS_START:
-      return {
-        ...state,
-        loading: true
-      };
-    case actionTypes.FETCH_ORDERS_SUCCESS:
-      return {
-        ...state,
-        orders: action.orders,
-        loading: false
-      };
-    case actionTypes.FETCH_ORDERS_FAIL:
-      return {
-        ...state,
-        loading: false
-      };
-    default:
-      return state;
+      });
+}
+
+const purchaseBurgerFail = (state, action) => {
+  return updateObject(state, {
+    loading: false,
+    error: action.error
+  });
+}
+
+const fetchOrderStart = (state, action) => {
+  return updateObject(state, {
+    loading: true
+  });
+}
+
+const fetchOrderSuccess = (state, action) => {
+  return updateObject(state, {
+    orders: action.orders,
+    loading: false
+  });
+}
+
+const fetchOrderFail = (state, action) => {
+  return updateObject(state, {
+    loading: false
+  });
+}
+
+const order = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.PURCHASE_INIT: return purchaseInit(state, action) ;
+    case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action);
+    case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state,action);
+    case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state, action);
+    case actionTypes.FETCH_ORDERS_START: return fetchOrderStart(state, action);
+    case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrderSuccess(state, action);
+    case actionTypes.FETCH_ORDERS_FAIL: return fetchOrderFail(state, action);
+    default: return state;
   }
 };
 
